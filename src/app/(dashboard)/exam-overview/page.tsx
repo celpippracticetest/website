410b751faccb39dead92b9203fa9c6ed853a2844
Amir Tejareh <a.tejareh@kian.digital>
2025-08-12 10:31:41 -0300

    feat: add metadata to the practice and exam overview page

--- src/app/(dashboard)/exam-overview/page.tsx
+++ src/app/(dashboard)/exam-overview/page.tsx
@@ -1,10 +1,27 @@
 import ExamOverview from "@/components/dashboard-app/examOverview";
 import mongoClient from "@/lib/mongodb";
 import { ExamRepository } from "@/repositories/exams.repo";
+import { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const appBaseUrl = process.env.APP_BASE_URL || "";
+  const isPreview = appBaseUrl.includes("vercel.app");
+  return {
+    title:
+      "CELPIP Mock Exam Overview: Practice Like the Real Test | CELPIP Practice Test",
+    alternates: {
+      canonical: "https://celpippracticetest.com/exam-overview",
+    },
+    robots: {
+      index: !isPreview,
+      follow: !isPreview,
+    },
+  };
+}
 
 const ExamsPage = async () => {
   const exampRepo = new ExamRepository(mongoClient);
-  const result = await exampRepo.getAllExam({isReady: true},0, 1000);
+  const result = await exampRepo.getAllExam({ isReady: true }, 0, 1000);
 
   return <ExamOverview exams={result.items} />;
 };
