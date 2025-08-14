90e18f79a276dfb5de3b6bc03ca6e4bdf3e7105c
amir tejareh <amirtejareh@gmail.com>
2025-08-14 17:08:20 +0330

    feat: develop referral page

--- src/components/dashboard-new/Referral.tsx
+++ src/components/dashboard-new/Referral.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useUser } from "@clerk/nextjs";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function Referral() {
   const { user } = useUser();
@@ -17,5 +18,27 @@ export default function Referral() {
 
   if (!user) return null;
 
-  return <></>;
+  return (
+    <div className="flex flex-col p-[16px] bg-[#F2F6FF] w-full">
+      <div className="relative bg-white px-[24px] py-[40px]">
+        <div className=" flex rounded-[16px] justify-between w-full h-[123px] bg-[#FDF4FF] px-[40px] py-[28px]">
+          <div className="flex flex-col gap-[12px]">
+            <span className="text-[#212E42] text-[29px] font-semibold">
+              Refer your friend and get money
+            </span>
+            <span className="text-[14px] font-normal text-[#76808F]">
+              Refer your friend and get money
+            </span>
+          </div>
+          <Image
+            alt="present"
+            width={165}
+            height={195}
+            className={`absolute top-0 right-[40px] w-[165px] h-[195px]`}
+            src="/images/present.png"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
