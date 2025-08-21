4266b374681cd3f36deaae4a6a38aa20f75010ce
amir tejareh <amirtejareh@gmail.com>
2025-08-21 14:15:54 +0330

    style(referral): change text color on hover via Tailwind group

--- src/components/dashboard-new/Referral.tsx
+++ src/components/dashboard-new/Referral.tsx
@@ -519,13 +519,13 @@ export default function Referral() {
                   );
                 }
               }}
-              className={`flex justify-center items-center mt-[24px] max-w-[188px] w-full h-[40px] border border-[#F27059] rounded-[24px] cursor-pointer transition-all ${
+              className={`flex justify-center items-center mt-[24px] max-w-[188px] w-full h-[40px] border border-[#F27059] rounded-[24px] cursor-pointer transition-all group ${
                 referralStats?.canWithdraw
                   ? "hover:bg-[#F27059] hover:!text-white"
                   : "opacity-50 cursor-not-allowed"
               }`}
             >
-              <span className="text-[14px] font-normal">
+              <span className="text-[14px] font-normal text-[#F27059] group-hover:!text-white">
                 {referralStats?.canWithdraw
                   ? "Transfer to my PayPal"
                   : "Insufficient Funds"}
