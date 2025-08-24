cefce6e5691e9c221071e77e5586f883f116a0c2
amir tejareh <amirtejareh@gmail.com>
2025-08-24 11:24:16 +0330

    fix

--- src/components/dashboard-app/writing-practice/answerModal.tsx
+++ src/components/dashboard-app/writing-practice/answerModal.tsx
@@ -64,7 +64,7 @@ const WritingAnswerModal = ({
           onClick={() => {
             setAnswerModalOpen(false);
           }}
-          className="fixed inset-0 z-50 bg-stone-300/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 transition-all duration-300"
+          className="fixed inset-0 z-[99] bg-stone-300/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 transition-all duration-300"
         ></div>
       )}
       {isAnswerModalOpen && (
