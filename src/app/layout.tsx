434c3fff5c1330feccddd3b3586be5cda525f78f
amir tejareh <amirtejareh@gmail.com>
2025-08-28 11:44:50 +0330

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ import Script from "next/script";
 import ReactQueryProvider from "@/components/ReactQueryProvider";
 import { LazyIntercom } from "@/components/LazyComponents";
 import PerformanceMonitor from "@/components/PerformanceMonitor";
-import CriticalCSS from "@/components/CriticalCSS";
 import { Metadata } from "next";
 
 const jakarta = Plus_Jakarta_Sans({
@@ -453,7 +452,6 @@ export default async function RootLayout({
           <PremiumPlanDrawer />
           <LazyIntercom />
           <PerformanceMonitor />
-          <CriticalCSS />
         </body>
       </html>
     </ClerkProvider>
