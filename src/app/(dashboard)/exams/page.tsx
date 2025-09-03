e3a4591c64aa831e803694004e025ffd78bc4f2a
Amir Tejareh <amirtejareh@gmail.com>
2025-09-03 09:29:57 -0300

    fix

--- src/app/(dashboard)/exams/page.tsx
+++ src/app/(dashboard)/exams/page.tsx
@@ -1,10 +1,9 @@
-
 const DashboardApp = () => {
   return (
     <div className="flex flex-col h-screen w-full bg-slate-100">
       <main className="flex-1 overflow-y-auto p-4 md:p-8 h-full   lg:pt-30 md:pt-20 pt-30">
-        <div className="max-w-6xl mx-auto">
-          <div  />
+        <div className="mx-auto">
+          <div />
         </div>
       </main>
     </div>
