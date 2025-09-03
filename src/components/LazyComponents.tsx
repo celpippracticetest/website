37348cc6a8dac485b3b28c84fb2f9c83cd7b1017
Amir Tejareh <amirtejareh@gmail.com>
2025-09-03 03:57:30 -0300

    fix

--- src/components/LazyComponents.tsx
+++ src/components/LazyComponents.tsx
@@ -12,7 +12,7 @@ export const LazyIntercom = dynamic(() => import("./IntercomLoader"), {
 // Generic loading component
 export const LoadingSpinner = () => (
   <div className="flex items-center justify-center h-32">
-    <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600"></div>
+    <div className="animate-spin rounded-full h-8 w-8 border-b-2 fixed border-blue-600"></div>
   </div>
 );
 
