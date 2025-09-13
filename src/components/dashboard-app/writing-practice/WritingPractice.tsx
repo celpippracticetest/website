c127464f4d51f2437efc467754f1def04acee03b
Amir Tejareh <amirtejareh@gmail.com>
2025-09-13 06:07:00 -0300

    style: update max-width of WritingPractice component for improved layout consistency

--- src/components/dashboard-app/writing-practice/WritingPractice.tsx
+++ src/components/dashboard-app/writing-practice/WritingPractice.tsx
@@ -64,7 +64,7 @@ const WritingPractice = ({
   };
 
   return selectedPractice ? (
-    <div className="flex flex-col  w-full ">
+    <div className="flex flex-col  w-full max-w-[1280px]">
       <div className="pl-[40px] text-[#76808F] text-[14px] mt-[24px] mb-[28px] items-center flex gap-[8px]">
         <div
           className="cursor-pointer"
