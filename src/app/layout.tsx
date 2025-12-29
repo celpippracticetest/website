85614df3a43460b4837619d3ad7a5f404369db15
mehrdokhtgohar <mehrdokht.gohard@gmail.com>
2025-12-29 09:38:11 +0330

    fix: removed old sw

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -108,22 +108,7 @@ export default async function RootLayout({
           />
           <link rel="manifest" href="/manifest.json" />
 
-          {/* Service Worker – lazy */}
-          <Script
-            id="sw-register"
-            strategy="lazyOnload"
-            dangerouslySetInnerHTML={{
-              __html: `
-                if ('serviceWorker' in navigator) {
-                  window.addEventListener('load', function() {
-                    navigator.serviceWorker.register('/sw.js')
-                      .then(function(reg) { console.log('SW registered:', reg); })
-                      .catch(function(err) { console.log('SW registration failed:', err); });
-                  });
-                }
-              `,
-            }}
-          />
+
 
           {/* JSON-LD early is fine */}
           <Script
