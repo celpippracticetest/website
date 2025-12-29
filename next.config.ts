70486906581b5cc9bbb05eb67413cd8925469f7b
mehrdokhtgohar <mehrdokht.gohard@gmail.com>
2025-12-29 18:54:40 +0330

    fix: removed rule on sw development

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,6 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
   // Workbox options
   buildExcludes: [/middleware-manifest\.json$/, /_buildManifest\.js$/, /_ssgManifest\.js$/],
   runtimeCaching: [
